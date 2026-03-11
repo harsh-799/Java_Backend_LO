90a958a4fbb57fe3e0bf81c4cafb8ea1c4a7abd1
harsh-799 <harshanand7898@gmail.com>
2026-03-11 19:34:04 +0530

    Junit: Lifecycle (@AfterEach annotation)

--- junitTesting/junitHub/src/main/java/org/junit/lifecycleAnnotation/LifecycleMethods.java
+++ junitTesting/junitHub/src/main/java/org/junit/lifecycleAnnotation/LifecycleMethods.java
@@ -1,17 +1,47 @@
 package org.junit.lifecycleAnnotation;
 
+class Database{
+
+    boolean isRunning = false;
+
+    public boolean turnOn(){
+        System.out.println("DB is running at xyz port");
+        if (!isRunning) {
+            isRunning = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean turnOff(){
+        if (isRunning) {
+            System.out.println("DB is turning Off..");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addData(String data){
+        if (isRunning){
+            System.out.println(data + "is added successfully..");
+            return true;
+        }
+        return false;
+    }
+}
+
 public class LifecycleMethods {
 
     // For BeforeEach
-    public boolean validUsername(String username){
+    public boolean validUsername(String username) {
         return username.length() > 6 && username.length() < 15 && username.contains("_");
     }
 
-    public boolean validPassword(String password){
+    public boolean validPassword(String password) {
         return password.length() > 6 && password.length() < 15 && password.contains("$");
     }
 
-    public boolean confirmPassword(String oldPassword,String password){
+    public boolean confirmPassword(String oldPassword, String password) {
         return oldPassword == password;
     }
 }
--- junitTesting/junitHub/src/test/java/org/junit/lifecycleAnnotation/LifecycleMethodsTestP2.java
+++ junitTesting/junitHub/src/test/java/org/junit/lifecycleAnnotation/LifecycleMethodsTestP2.java
@@ -1,13 +1,20 @@
 package org.junit.lifecycleAnnotation;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LifecycleMethodsTestP2 {
 
     LifecycleMethods lc;
+    Database db;
 
     @Test
     void validUsername() {
@@ -27,9 +34,41 @@ class LifecycleMethodsTestP2 {
     // 🏞️ @beforeEach() {it tells that before every test the flowing stuffs will execute}
 
     // Like for this earlier we used to create obj manually everytime, but now we're using beforeEach which will perform the set of instruction defined inside the method inside @beforeEach notatted method.
-    @Test
+
     @BeforeEach  //
     void instanceCreation(){
         lc = new LifecycleMethods();
     }
+
+    // 🏞️ @afterEach() {it tells that after every test this following stuffs will execute every time}
+
+    // We might use it in closing resouce
+
+    @BeforeEach
+    void createDB(){
+        db = new Database();
+    }
+
+    @Test
+    void insertData(){
+        assertAll(
+                ()->assertTrue(db.turnOn(),()->"DB is already in USE"),
+                ()->assertTrue(db.addData("Harsh"))
+                );
+    }
+
+    @AfterEach
+    void closeDB(){
+        db.turnOff();
+        db = null;
+    }
+
+    /*
+    - DB is running at xyz port
+    - Harshis added successfully..
+    - DB is turning Off..
+     */
+
 }
+
+
