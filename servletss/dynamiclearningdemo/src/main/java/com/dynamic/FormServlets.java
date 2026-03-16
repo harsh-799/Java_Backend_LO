10ee9a83013c09accda5c09583be7a470124748a
harsh-799 <harshanand7898@gmail.com>
2026-03-16 19:32:16 +0530

    Servlets: Sending Resp using writer (HTML)
    - includes why GET Method is bad

--- servletss/dynamiclearningdemo/src/main/java/com/dynamic/FormServlets.java
+++ servletss/dynamiclearningdemo/src/main/java/com/dynamic/FormServlets.java
@@ -14,9 +14,32 @@ public class FormServlets extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         String city = req.getParameter("city");
+        String ageStr = req.getParameter("age"); // ⚠️ Even though age is number in Html use getParamter() everything is converted to string.
+
+        Integer age = Integer.parseInt(ageStr);
 
         PrintWriter writer = resp.getWriter();
 
-        writer.println("Your name is "+name + " and you're from "+city);
+        // Normal printing on Screen
+        // if (age >= 18){
+        //     writer.println("Your name is "+name);
+        //     writer.println("Your age is" + age);
+        //     writer.println(" You're from "+city);
+        //     writer.println("Congrats You're allowed inside to webpage");
+        // } else {
+        //     writer.println("Bada hoo ja pahele");
+        // }
+
+        // Instead, we can also send resp using HTML format
+        if (age >= 18){
+            writer.println("<h1> Your name is "+name+"</h1>");
+            writer.println("<h1> Your age is" + age+ "</h1>");
+            writer.println("<h1> You're from "+city+ "</h1>");
+            writer.println("<h1> Congrats You're allowed inside to webpage ✅ </h1>");
+        } else {
+            writer.println("<h1 style='color: red'>Bada hoo ja pahele</h1>");
+        }
+
+
     }
 }
