faa62ec24e714de8f36199d564862de1c1e263bd
harsh-799 <harshanand7898@gmail.com>
2026-03-27 19:13:27 +0530

    Spring: Application Context and beans.xml
    - includes syntax of ApplicationContext and beans.xml

---
+++ Hibernate/SpringPart1/src/main/java/com/harsh/springcore/basics/app/BasicsOfApplicationContext.java
@@ -0,0 +1,19 @@
+package com.harsh.springcore.basics.app;
+
+import com.harsh.springcore.basics.model.Car;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BasicsOfApplicationContext {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        // Car carObj = context.getBean("carkiid"); // 🧾 getBean() retuns object type so we need tio type cast it.
+        Car carObj = (Car) context.getBean("carkiid");
+        carObj.setBrandName("Toyata");
+        carObj.setPrice(10000);
+
+        System.out.println(carObj); // ✅ Car{brandName='Toyata', price=10000}
+
+
+    }
+}
---
+++ Hibernate/SpringPart1/src/main/java/com/harsh/springcore/basics/model/Car.java
@@ -0,0 +1,35 @@
+package com.harsh.springcore.basics.model;
+
+public class Car {
+
+    String brandName;
+    int price;
+
+    public Car() {
+
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brandName='" + brandName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
