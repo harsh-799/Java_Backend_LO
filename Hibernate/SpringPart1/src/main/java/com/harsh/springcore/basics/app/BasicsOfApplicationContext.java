a64bcdfb422693eb10f057f517e4cc251716bcc9
harsh-799 <harshanand7898@gmail.com>
2026-03-27 19:38:44 +0530

    Spring: Bean Scope I (Singleton)

--- Hibernate/SpringPart1/src/main/java/com/harsh/springcore/basics/app/BasicsOfApplicationContext.java
+++ Hibernate/SpringPart1/src/main/java/com/harsh/springcore/basics/app/BasicsOfApplicationContext.java
@@ -1,6 +1,7 @@
 package com.harsh.springcore.basics.app;
 
 import com.harsh.springcore.basics.model.Car;
+import com.harsh.springcore.basics.model.Engine;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -27,8 +28,46 @@ public class BasicsOfApplicationContext {
 
         // 🩹 Even if we repeat two beans with same id it'll be creatiing two times not just one time.
     }
+
+    public static void beanScopeSingleton() {
+    /*    📘 2. Scope of Beans (SUPER IMPORTANT)
+    - “How many objects Spring will create”
+
+    🔹 1. Singleton (Default) ⭐
+    👉 Only ONE object in whole application
+    🧠 Meaning
+    👉 Spring creates only ONE object and reuses it
+     */
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Car c0 = (Car) context.getBean("carkiid");
+        c0.setBrandName("Maruti");
+
+        Car c1 = (Car) context.getBean("carkiid");
+        System.out.println(c0.getBrandName()); // ✅ Maruti
+        System.out.println(c1.getBrandName()); // ✅ Maruti (See we didn't even setted the brandName of the c1 object still it's showing as Maruti)
+
+        /* OUTPUT ✅
+        Car object is created..
+        Maruti
+        Maruti
+
+        🧙‍♂️ So Using id named as carkiid only 1 object is created by ApplicationContext It doesn't matter how many Ref assignation/creation to it.
+         */
+
+        // Case 2: (Using diff ID)
+        Car c2 = (Car) context.getBean("carkiid1");
+        System.out.println(c2.getBrandName()); // ✅ null
+
+        /*
+         🌙 So Conclusion if we use same ID and we create diff Ref to them then all the ref will be pointing to the same object only.
+         🌙 If the Id will be diff then the Ref will be diff
+         */
+    }
+
     public static void main(String[] args) {
         // creatingApplicationContext(); 📌 See we' have commented it
-        seeingWhenObjectIsCreatedByContainer();
+        // seeingWhenObjectIsCreatedByContainer();
+        // beanScopeSingleton();
     }
 }
