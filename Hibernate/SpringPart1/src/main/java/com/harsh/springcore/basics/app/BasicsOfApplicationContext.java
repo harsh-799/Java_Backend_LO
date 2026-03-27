c1ab7474c7358285c839dafc28b8346557fe9e43
harsh-799 <harshanand7898@gmail.com>
2026-03-27 19:26:06 +0530

    Spring: Object Creation in Application Context
    - include when the object gets created, how many times it gets created

--- Hibernate/SpringPart1/src/main/java/com/harsh/springcore/basics/app/BasicsOfApplicationContext.java
+++ Hibernate/SpringPart1/src/main/java/com/harsh/springcore/basics/app/BasicsOfApplicationContext.java
@@ -15,7 +15,20 @@ public class BasicsOfApplicationContext {
 
         System.out.println(carObj); // ✅ Car{brandName='Toyata', price=10000}
     }
+
+    public static void seeingWhenObjectIsCreatedByContainer() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        // 🧑‍🔬 We're not callling any Object By id till now we're just loading the beans.xml at the application startup to just to see when the object is created at the time of Calling by id or at the running of ApplicationContext.
+
+        // ✅ Car object is created..
+        // ✅ Engine Object created.
+
+        // 🩹 SO it'll get called when the Application context is init and the number of object created depending upon how many beans is declared over there.
+
+        // 🩹 Even if we repeat two beans with same id it'll be creatiing two times not just one time.
+    }
     public static void main(String[] args) {
-        creatingApplicationContext();
+        // creatingApplicationContext(); 📌 See we' have commented it
+        seeingWhenObjectIsCreatedByContainer();
     }
 }
