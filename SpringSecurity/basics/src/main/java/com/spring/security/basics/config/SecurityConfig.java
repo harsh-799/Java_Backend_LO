cf2cdd31e6b61d7629cb160ff4e4f13e24aa9657
harsh-799 <harshanand7898@gmail.com>
2026-04-15 19:58:16 +0530

    Spring_Sec: Specifying Particular EndPoints to be Authorized
    - Note taht if we give id and pass then it'll authenticate for sure.
    - but w/o it it'll work too.

--- SpringSecurity/basics/src/main/java/com/spring/security/basics/config/SecurityConfig.java
+++ SpringSecurity/basics/src/main/java/com/spring/security/basics/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.spring.security.basics.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,7 +24,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/labours").permitAll() // This means /labours end point doesn't require auth NOTE: Don't think RequestMatchers means /labour/any endPoint like /labour/hi, /labour/bye will work it explictly mean to /labour
+                        .anyRequest().authenticated() // whereas anyRequest other than that needs to be authenticated
+                )
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
