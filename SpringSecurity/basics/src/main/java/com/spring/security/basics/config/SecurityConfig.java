1148e05c50f899a0a7c5508987364ffc875696dc
harsh-799 <harshanand7898@gmail.com>
2026-04-16 10:49:43 +0530

    Spring_Sec: Added Custom Users
    - As soon as we add custom users the default user credentials won't work Anymore as current one overrides the previous one
    - ISSUE ! Now the issue in this we don't have role checking like who can see what so we need to have authorization concept here

--- SpringSecurity/basics/src/main/java/com/spring/security/basics/config/SecurityConfig.java
+++ SpringSecurity/basics/src/main/java/com/spring/security/basics/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /*
@@ -32,6 +36,27 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    // Since till now we were able to Use the Backend with Single User Now we're declaring more than 1 user.
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User
+                .withUsername("Kartik")
+                .password("{noop}12345")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User
+                .withUsername("Builder")
+                .password("{noop}builder")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
     }
 }
 
