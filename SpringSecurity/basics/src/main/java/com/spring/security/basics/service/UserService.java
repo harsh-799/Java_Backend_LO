0bee57d5005a96f19328189890feadf501c12950
harsh-799 <harshanand7898@gmail.com>
2026-04-18 08:38:03 +0530

    Spring_Sec: Encoded Password Using Bcrypt (REGISTER)
    - implemneted currently now while Registering the User.
    - In the next we'll see how we'll compare the raw password with encoded one in DB and we'll be checking not by some /login (EndPoint) Just Basic Auth Way

--- SpringSecurity/basics/src/main/java/com/spring/security/basics/service/UserService.java
+++ SpringSecurity/basics/src/main/java/com/spring/security/basics/service/UserService.java
@@ -3,19 +3,23 @@ package com.spring.security.basics.service;
 import com.spring.security.basics.model.User;
 import com.spring.security.basics.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void addNewUsers(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 }
