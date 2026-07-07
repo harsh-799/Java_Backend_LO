df98d1668ded6e4b1a8b4821737b17b3e7478655
harsh-799 <harshanand7898@gmail.com>
2026-07-07 19:15:29 +0530

    ms: project setup

---
+++ Microservices/ecom-inventoryservice/src/main/java/com/harsh/microservices/ecom_inventoryservice/EcomInventoryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.microservices.ecom_inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomInventoryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomInventoryserviceApplication.class, args);
+	}
+
+}
---
+++ Microservices/ecom-inventoryservice/src/test/java/com/harsh/microservices/ecom_inventoryservice/EcomInventoryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.microservices.ecom_inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomInventoryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
